0a61a165f0988aaeadb658b64ce1c35debf74022
anxieuse <64018836+anxieuse@users.noreply.github.com>
2021-03-19 14:52:14 +0300

    Add files via upload

---
+++ debug_template.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// braindead
+using ll = long long;
+#define int ll
+
+// types
+using ld = long double;
+using ull = unsigned long long;
+
+// vectors 
+using vi = vector<int>;
+using vd = vector<double>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+
+// others 
+using pii = pair<int,int>;
+ 
+// shorthand
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+ 
+// process a container
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define sz(a) int((a).size())
+ 
+// loops
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)            // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)           // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interval [l; r]
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)        // reverse 0 based
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)          // reverse 1 based
+#define trav(a, x) for (auto &a : x)
+ 
+// other
+#define endl '\n'
+#define fast_io              \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+ 
+const ll inf = LLONG_MAX / 3;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#define LOCAL 
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+signed main()
+{
+    fast_io;
+ 
+    int x;
+    cin >> x;
+    debug(x);
+}
---
+++ template.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// braindead
+using ll = long long;
+#define int ll
+
+// types
+using ld = long double;
+using ull = unsigned long long;
+
+// vectors 
+using vi = vector<int>;
+using vd = vector<double>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+
+// others 
+using pii = pair<int,int>;
+ 
+// shorthand
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+ 
+// process a container
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define sz(a) int((a).size())
+ 
+// loops
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)            // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)           // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interval [l; r]
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)        // reverse 0 based
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)          // reverse 1 based
+#define trav(a, x) for (auto &a : x)
+ 
+// others
+#define endl '\n'
+#define fast_io              \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+ 
+const ll INF = LLONG_MAX / 3;
+
+signed main()
+{
+    fast_io;
+    
+}
